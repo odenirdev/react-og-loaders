9b7748f58ee1c5a87f39362dbc17cabfe18191e0
Odenir <odenir.filho@sagris.com.br>
2021-03-29 10:12:54 -0300

    v1.1.0

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -12,13 +12,16 @@ const spin = keyframes`
 interface Props {
   color?: string
   size?: number
+  borderSize?: number
 }
 
-export default styled.div<Props>`
-  border: 8px solid rgba(0, 0, 0, 0.1);
-  border-left: 8px solid ${(props) => props.color || 'black'};
-  height: ${(props) => props.size || '60'}px;
-  width: ${(props) => props.size || '60'}px;
+export default styled.div.attrs({
+  className: 'react-og-spinner'
+})`
+  border: ${(props: Props) => props.borderSize || 8}px solid rgba(0, 0, 0, 0.1);
+  border-left: 8px solid ${(props: Props) => props.color || 'black'};
+  height: ${(props: Props) => props.size || '60'}px;
+  width: ${(props: Props) => props.size || '60'}px;
   border-radius: 50px;
   animation: ${spin} 1s linear infinite;
 `
